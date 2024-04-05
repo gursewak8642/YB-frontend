3b2c0ab7a1d63cc3befc5239026e9197fd4b1661
gursewak8642 <gursewak8642@gmail.com>
2024-04-05 20:59:31 +0530

    frontend Code

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -8,9 +8,9 @@ import { ProductCollectionWithPreviews } from "types/global"
 import { cache } from "react"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "Young & Bold : Unleash Your Style ",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "Young, Wild, and Absolutely Chic: Welcome to Young and Bold – the epitome of youthful exuberance in fashion. Browse through our eclectic range of clothing designed to empower the bold and celebrate the spirited. It's time to embrace your individuality!",
 }
 
 const getCollectionsWithProducts = cache(
